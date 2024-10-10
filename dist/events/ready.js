11835231792d690ce0039183fa324cfdf41cf1e9
Guilherme Gomes David <guidev001@gmail.com>
2024-10-10 20:03:33 -0300

    fix: remove dist folder

--- dist/events/ready.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = ready;
-function ready(client) {
-    client.once('ready', () => {
-        console.log(`Bot is logged in as ${client.user?.tag}`);
-    });
-}
